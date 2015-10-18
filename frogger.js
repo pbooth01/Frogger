6f06564def6b661b6a71f19f3d8960be83acb536
Phil Booth <phillip.booth2015@gmail.com>
2015-10-18 11:46:55 -0400

    made code a litrle cleaner/added more background

--- frogger.js
+++ frogger.js
@@ -1,32 +1,16 @@
 
 function start_game(){
 	var canvas = $("#game")[0];
-	var points = 0;
+	var score = 0;
 	var lives = 5;
 	var level = 1;
 	var counter = 0;
 	var wins = 0;
-	var girl = 0;
 	var frog = {xPos: 180 , yPos: 475 };
 
 
 	if(canvas.getContext){
 		var ctx = canvas.getContext("2d");
-
-		
-		var x1 = 130;
-		var x2 = 130;
-		var x3 = 130;
-		var x4 = 130;
-		var x5 = 130;
-		var y1 = 395;
-		var y2 = 395;
-		var y3 = 395;
-		var y4 = 395;
-		var y5 = 395;
-
-
-
 
 		renderBackground();
 
@@ -39,54 +23,49 @@ function start_game(){
 		    ctx.fillStyle = "rgb(00, 00, 00)";
 		    ctx.fillRect(0, 282, 399, 282);
 
-		    /*Grabbing Sprite sheet
-		    var spritePromise = new Promise(function(resolve, reject){
-		    	while(!image.)
-		    });
-			*/
 			var img = new Image();
 			img.src = 'images/frogger_sprites.png';
 
 			/*
-			 *Draw the landing pads 
 			 *So I remeber what each parameter stands for. Gonna be using drawImage a lot!
 			 *sx source x(Top Left) dx destination x(Top Left)
-			 *ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, 
+			 *ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight 
 			 */
 			img.onload = function(){
-				ctx.drawImage(img, x1, y1, 35, 35, 5, 80, 30, 35);
-				ctx.drawImage(img, x2, y2, 35, 35, 93, 80, 30, 35);
-				ctx.drawImage(img, x3, y3, 35, 35, 181, 80, 30, 35);
-				ctx.drawImage(img, x4, y4, 35, 35, 269, 80, 30, 35);
-				ctx.drawImage(img, x5, y5, 35, 35, 355, 80, 30, 35);
 
 
-				//lives
-				if(lives== 1){
-					ctx.drawImage(img, 10, 325, 30, 30, 0, 515, 20, 20);
+				//Draw the turtles
+				var turtleImageCoords = {xPos: 130, yPos: 395};
+
+				ctx.drawImage(img, turtleImageCoords.xPos, turtleImageCoords.yPos, 35, 35, 5, 80, 30, 35);
+				ctx.drawImage(img, turtleImageCoords.xPos, turtleImageCoords.yPos, 35, 35, 93, 80, 30, 35);
+				ctx.drawImage(img, turtleImageCoords.xPos, turtleImageCoords.yPos, 35, 35, 181, 80, 30, 35);
+				ctx.drawImage(img, turtleImageCoords.xPos, turtleImageCoords.yPos, 35, 35, 269, 80, 30, 35);
+				ctx.drawImage(img, turtleImageCoords.xPos, turtleImageCoords.yPos, 35, 35, 355, 80, 30, 35);
+
+
+				//using number of lives to draw the frogs at the bottom
+				var frogXPos = 0;
+				for(var i = 0; i < lives; i++){
+					ctx.drawImage(img, 10, 325, 30, 30, frogXPos, 515, 20, 20);
+					frogXPos += 15;
 				}
-				if(lives == 2){
-					ctx.drawImage(img, 10, 325, 30, 30, 0, 515, 20, 20);
-					ctx.drawImage(img, 10, 325, 30, 30, 15, 515, 20, 20);
-				}
-				if(lives == 3){
-					ctx.drawImage(img, 10, 325, 30, 30, 0, 515, 20, 20);
-					ctx.drawImage(img, 10, 325, 30, 30, 15, 515, 20, 20);
-					ctx.drawImage(img, 10, 325, 30, 30, 30, 515, 20, 20);
-				}
-				if(lives == 4){
-					ctx.drawImage(img, 10, 325, 30, 30, 0, 515, 20, 20);
-					ctx.drawImage(img, 10, 325, 30, 30, 15, 515, 20, 20);
-					ctx.drawImage(img, 10, 325, 30, 30, 30, 515, 20, 20);
-					ctx.drawImage(img, 10, 325, 30, 30, 45, 515, 20, 20);
-				}
-				if(lives == 5){
-					ctx.drawImage(img, 10, 325, 30, 30, 0, 515, 20, 20);
-					ctx.drawImage(img, 10, 325, 30, 30, 15, 515, 20, 20);
-					ctx.drawImage(img, 10, 325, 30, 30, 30, 515, 20, 20);
-					ctx.drawImage(img, 10, 325, 30, 30, 45, 515, 20, 20);
-					ctx.drawImage(img, 10, 325, 30, 30, 60, 515, 20, 20);
-				}
+
+
+				// Grabbing both Frogger and the green map ending 
+				ctx.drawImage(img, 0, 0, 399, 100, -5, 5, 410, 115);
+
+				//Text at the bottom of te canvas
+				ctx.fillStyle="rgb(0, 255, 0)";
+				ctx.font="30px Verdana";
+				ctx.fillText("Level " + level, 150, 535);
+			
+				ctx.font="11px Verdana";
+				ctx.fillText("Score: "+ points +"	Highscore: 0", 0, 555);
+
+				// Put in purple strips	
+				ctx.drawImage(img, 0, 120, 399, 30, 0, 283, 399, 30);
+				ctx.drawImage(img, 0, 120, 399, 30, 0, 475, 399, 30);
 
 			}
 
